c17a1e9205c7abefe38bcd19c41faebe4c76da1f
D-Titan <111182645+D-Titan@users.noreply.github.com>
2026-04-18 14:24:03 +0530

    Refactor caching to use requests.put directly
    Replaced custom put function with requests.put for caching.

--- DailyReports.py
+++ DailyReports.py
@@ -23,8 +23,6 @@ from jinja2 import Template
 def fetch(link : str, headers : dict = { "User-Agent": "Mozilla/5.0", "Accept": "application/json" }, params : dict = {}) :
     return requests.get(link, headers = headers).content
 
-def put(link : str, headers : dict = { "User-Agent": "Mozilla/5.0", "Accept": "application/json" }, data) :
-    return requests.put(link, headers = headers, data=data).content
 
 def getSubs() :
     brevo = "https://api.brevo.com/v3/contacts"
@@ -773,7 +771,7 @@ finalHTML = template.render(date = date, time = time, ipotable = ipoTable, moreI
 
 
 #Caching this report 
-cacheResponse = put(cacheURL, headers = { "Authorization": f"Bearer {cacheAPI}", "Content-Type": "text/plain" }, data = finalHTML)
+cacheResponse = requests.put(cacheURL, headers = { "Authorization": f"Bearer {cacheAPI}", "Content-Type": "text/plain" }, data = finalHTML)
 print("Cache Status:" + cacheResponse.status_code)
 
 # Finally sending emails
